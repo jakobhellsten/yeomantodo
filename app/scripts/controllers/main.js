c3a3f1f999cb6c951d9bc184487fd24b8fc86f65
Jakob Hellsten <jakob.hellsten@gmail.com>
2015-09-16 09:58:25 +0200

    update for tests and localstorage

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,16 +8,22 @@
  * Controller of the yeomantodoApp
  */
 angular.module('yeomantodoApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.todos = [
-      'Item1',
-      'Item2',
-      'Item3',
-      'WTF'
-    ];
+  .controller('MainCtrl', function ($scope, localStorageService) {
+    
+    var todosInStore = localStorageService.get('todos');
+    
+    $scope.todos = todosInStore || [];
+    
+    $scope.$watch('todos', function(){
+      localStorageService.set('todos', $scope.todos);      
+    }, true);
     
     $scope.addTodo = function() {
       $scope.todos.push($scope.todo);
       $scope.todo = '';
+    };
+    
+    $scope.removeTodo = function(index){
+      $scope.todos.splice(index, 1);
     };
   });
